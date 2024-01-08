b14f78d464fdacf902ec08dfe7080b74926886a9
Irfansk007 <irfanshaikh2852000@gmail.com>
2024-01-08 17:32:51 +0530

    second

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+
 import UserRoute from "./Routes/UserRoute.js";
 
 import cookieParser from "cookie-parser";
 
 //app initialization
 const app = express();
-dotenv.config();
+
 
 //middleware
 app.use(express.json());
@@ -21,7 +21,9 @@ app.use("/auth", UserRoute);
 
 //server initialization
 const port = process.env.PORT || 8000;
-
+app.get('/',async(req,res)=>{
+res.json({messege:"server is running"})
+})
 app.listen(port, () => {
   console.log(`server is running on port http://localhost:${port}`);
 });
